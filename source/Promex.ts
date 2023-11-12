52fda313c0c8792631e4c108b193afd58f2ed5be
temich <tema.gurtovoy@gmail.com>
2023-11-12 10:04:27 +0400

    fix: add support for `resolve()` without argument

--- source/Promex.ts
+++ source/Promex.ts
@@ -1,29 +1,29 @@
-export class Promex<T = any> extends Promise<T> {
+export class Promex<T> extends Promise<T> {
   public resolve: Resolution<T>
   public reject: Rejection
 
-  public constructor (executor: (resolve: Resolution<T>, reject: Rejection) => void = () => {}) {
-    let ok = undefined as unknown as Resolution<T>
-    let oh = undefined as unknown as Rejection
+  public constructor (executor = (..._: any[]) => {}) {
+    let ok
+    let oh
 
-    super((resolve: Resolution<T>, reject: Rejection) => {
+    super((resolve, reject) => {
       ok = resolve
       oh = reject
 
       return executor(resolve, reject)
     })
 
-    this.resolve = ok
-    this.reject = oh
+    this.resolve = ok as unknown as Resolution<T>
+    this.reject = oh as unknown as Rejection
   }
 
   public callback = (err: any, value: T): void => {
     if (err !== null && err !== undefined)
-      this.reject(err)
+      void this.reject(err)
     else
-      this.resolve(value)
+      void this.resolve(value)
   }
 }
 
-type Resolution<T> = (value: T | PromiseLike<T>) => void
+type Resolution<T> = (value?: T | PromiseLike<T>) => void
 type Rejection = (reason?: any) => void
